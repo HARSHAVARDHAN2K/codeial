a7681b1634088acbf57b9406465de2c1fc8a37c1
Harshavardhan <vardhanvardhanthi@gmail.com>
2022-10-02 19:49:15 +0530

    add a controller and a router

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express (Middle ware)
+app.use('/',require('./routes/index'));
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${port}`);
