f78e9c5315d12dbb2333cfc0ae3f97a3dbd7dd79
Harshavardhan <vardhanvardhanthi@gmail.com>
2022-10-13 08:49:33 +0530

    comments and mongoose populate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,24 @@
-module.exports.home  = function(req,res){
-     console.log(req.cookies);
-     res.cookie('user_id', 25);
-     return res.render('home',{
-        title : "HOME",
-     })
-}
+const Post = require("../models/post");
+module.exports.home = function (req, res) {
+  //   console.log(req.cookies);
+  //   res.cookie('user_id', 25);
+
+//   Post.find({}, function (err, posts) {
+//     return res.render("home", {
+//       title: "Codeila|HOME",
+//       posts: posts,
+
+//     });
+//   });
+
+
+  //populate the user of each comment
+  Post.find({}).populate('user').exec(function(err, posts){
+   return res.render("home", {
+      title: "Codeila|HOME",
+      posts: posts,
+
+    });
+  });
+
+};
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const homecontroller = require('../controllers/home_controller');
 router.get('/', homecontroller.home);
 //whenever we find the /users details this index access users router
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 
 //exporting the router
 module.exports = router;
